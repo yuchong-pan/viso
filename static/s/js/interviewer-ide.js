24234ecc2f26845c4106c8586a59120ce4edbfef
yuchong-pan <panyuchong@gmail.com>
2017-05-14 03:04:08 +0800

    Fixed code sync issue

--- static/s/js/interviewer-ide.js
+++ static/s/js/interviewer-ide.js
@@ -1,10 +1,14 @@
 var socket = io.connect("http://viso.hackinit.io/LP01632947305");
 socket.on("modify", function(data) {
-    editor.setValue(data, 0);
+    if (editor.getValue() != data) {
+        editor.setValue(data, 0);
+    }
 });
 editor.on("change", function() {
-    Vue.http.post("http://viso.hackinit.io/api/ide/modify", {
-        "code": editor.getValue(),
-        "author": "LP01632947305"
-    });
+    if (editor.getValue() != "") {
+        Vue.http.post("http://viso.hackinit.io/api/ide/modify", {
+            "code": editor.getValue(),
+            "author": "LP01632947305"
+        });
+    }
 });
