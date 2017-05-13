3ecb94e9198af0c614b32b640eb7f30c9f4b8593
yuchong-pan <panyuchong@gmail.com>
2017-05-14 03:29:23 +0800

    Fixed cursor & selection sync issues

--- static/s/js/interviewee-ide.js
+++ static/s/js/interviewee-ide.js
@@ -1,10 +1,10 @@
 var socket = io.connect("http://viso.hackinit.io/interviewee");
 editor.on("change", function() {
-    if (editor.getValue() != "") {
+    window.setTimeout(function() {
         Vue.http.post("http://viso.hackinit.io/api/ide/modify", {
             "code": editor.getValue(),
             "pos": editor.getCursorPosition(),
             "select": editor.getSelection().rangeList.ranges
         });
-    }
+    }, 50);
 });
