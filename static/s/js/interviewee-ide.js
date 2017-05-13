e537b5ce7d7c09c875b6104586587cc301bf7404
yuchong-pan <panyuchong@gmail.com>
2017-05-14 02:58:55 +0800

    Fixed code sync issue

--- static/s/js/interviewee-ide.js
+++ static/s/js/interviewee-ide.js
@@ -1,6 +1,8 @@
 var socket = io.connect("http://viso.hackinit.io/interviewee");
 socket.on("modify", function(data) {
-    editor.setValue(data, 0);
+    if (editor.getValue() != data) {
+        editor.setValue(data, 0);
+    }
 });
 editor.on("change", function() {
     Vue.http.post("http://viso.hackinit.io/api/ide/modify", {
