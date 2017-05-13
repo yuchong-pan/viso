3976115cd5673f93c52b2d9963562d34c8d1421b
yuchong-pan <panyuchong@gmail.com>
2017-05-14 02:17:55 +0800

    Set debug mode

--- flask/app.py
+++ flask/app.py
@@ -22,4 +22,4 @@ def code_modified():
     return "code changed from: " + namespace
 
 if __name__ == "__main__":
-    socketio.run(app, host="0.0.0.0")
+    socketio.run(app, host="0.0.0.0", debug=True)
