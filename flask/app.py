db6ab7e91fbe5eed4efd7235949b005626d05388
yuchong-pan <panyuchong@gmail.com>
2017-05-13 23:37:45 +0800

    Updated server-side code

--- flask/app.py
+++ flask/app.py
@@ -1,39 +1,15 @@
-import os
-from flask import Flask, jsonify, request
-from faker import Factory
-from twilio.rest import Client
-from twilio.jwt.access_token import AccessToken
-from twilio.jwt.access_token.grants import (
-    IpMessagingGrant,
-    SyncGrant
-)
+from flask import Flask
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
-fake = Factory.create()
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
-account_sid = os.environ["TWILIO_ACCOUNT_SID"]
-api_key = os.environ["TWILIO_API_KEY"]
-api_secret = os.environ["TWILIO_API_SECRET"]
-chat_service_sid = os.environ["TWILIO_CHAT_SERVICE_SID"]
-sync_service_sid = os.environ["TWILIO_SYNC_SERVICE_SID"]
-
-def access_token():
-    identity = request.args.get("id", fake.user_name())
-    token = AccessToken(account_sid, api_key, api_secret, identity=identity)
-
-@app.route("/chat/token")
-def chat_token():
-    token = access_token()
-    chat_grant = IpMessagingGrant(service_sid=chat_service_sid)
-    token.add_grant(chat_grant)
-    return jsonify(identity=identity, token=token.to_jwt())
-
-@app.route("/sync/token")
-def sync_token():
-    token = access_token()
-    sync_grant = IpMessagingGrant(service_sid=sync_service_sid)
-    token.add_grant(sync_grant)
-    return jsonify(identity=identity, token=token.to_jwt())
+@app.route("/api/leap/alert")
+def received_alert():
+    namespace = "/" + request.args.get("id", "")
+    console.log("received alert from: " + namespace)
+    emit("alert", "!", namespace=namespace)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app)
